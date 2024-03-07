54c69dc11020c44702daad0f0267ae766e5aea31
Begmyradov Myratgeldi <begmyradowmyratgeldi@gmail.com>
2024-03-07 22:21:27 +0500

    road to success

--- ratings/serializers.py
+++ ratings/serializers.py
@@ -1,32 +1,32 @@
-# from rest_framework import serializers
-# from .models import Rate_User
-# from users.models import User
+from rest_framework import serializers
+from .models import Rate_User
+from users.models import User
 
 
-# class User_CommentsSerializer(serializers.ModelSerializer):
-#     commented_user_username = serializers.SerializerMethodField(read_only=True)
-#     commented_user_avatar = serializers.SerializerMethodField(read_only=True)
+class User_CommentsSerializer(serializers.ModelSerializer):
+    commented_user_username = serializers.SerializerMethodField(read_only=True)
+    commented_user_avatar = serializers.SerializerMethodField(read_only=True)
 
-#     @staticmethod
-#     def get_commented_user_username(instance):
-#         return User.objects.get(id=instance.rating_user.id).username
+    @staticmethod
+    def get_commented_user_username(instance):
+        return User.objects.get(id=instance.rating_user.id).username
     
-#     @staticmethod
-#     def get_commented_user_avatar(instance):
-#         return User.objects.get(id=instance.rating_user.id).avatar.url
+    @staticmethod
+    def get_commented_user_avatar(instance):
+        return User.objects.get(id=instance.rating_user.id).avatar.url
     
-#     class Meta:
-#         model = Rate_User
-#         fields = "__all__"
+    class Meta:
+        model = Rate_User
+        fields = "__all__"
 
 
-# class User_data_Serializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = User
-#         fields = ["username", "avatar", "rate_point"]
+class User_data_Serializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["username", "avatar", "rate_point"]
 
 
-# class Leave_Comment_Serializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Rate_User
-#         fields = "__all__"
+class Leave_Comment_Serializer(serializers.ModelSerializer):
+    class Meta:
+        model = Rate_User
+        fields = "__all__"
--- ratings/urls.py
+++ ratings/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 
 
 urlpatterns = [
-    # path('user_ratings/<int:pk>', views.User_CommentsAPIView().as_view(), name='user_rates'),
-    # path('user_ratings', views.Leave_CommentAPIView().as_view(), name='leave_comment'),
+    path('user_ratings/<int:pk>', views.User_CommentsAPIView().as_view(), name='user_rates'),
+    path('user_ratings', views.Leave_CommentAPIView().as_view(), name='leave_comment'),
 ]
