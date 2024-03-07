8cf12820f88733ff9a4e77b7b0747f88c30a12f0
Begmyradov Myratgeldi <begmyradowmyratgeldi@gmail.com>
2024-03-07 21:58:00 +0500

    uncomment user

--- ratings/admin.py
+++ ratings/admin.py
@@ -1,8 +1,8 @@
-from django.contrib import admin
-from .models import *
+# from django.contrib import admin
+# from .models import *
 
-admin.site.register(Rate_User)
-admin.site.register(Like_Service)
-admin.site.register(View_User)
-admin.site.register(Like_User)
-admin.site.register(View_Service)
+# admin.site.register(Rate_User)
+# admin.site.register(Like_Service)
+# admin.site.register(View_User)
+# admin.site.register(Like_User)
+# admin.site.register(View_Service)
--- ratings/models.py
+++ ratings/models.py
@@ -2,33 +2,33 @@ from django.db import models
 from django.utils import timezone
 
 
-class Like_Service(models.Model):
-    user = models.ForeignKey("users.User", on_delete=models.CASCADE)
-    service = models.ForeignKey("services.Service", on_delete=models.CASCADE)
-    date_created = models.DateTimeField(default=timezone.now)
+# class Like_Service(models.Model):
+#     user = models.ForeignKey("users.User", on_delete=models.CASCADE)
+#     service = models.ForeignKey("services.Service", on_delete=models.CASCADE)
+#     date_created = models.DateTimeField(default=timezone.now)
 
 
-class View_Service(models.Model):
-    user = models.ForeignKey("users.User", on_delete=models.CASCADE)
-    service = models.ForeignKey("services.Service", on_delete=models.CASCADE)
+# class View_Service(models.Model):
+#     user = models.ForeignKey("users.User", on_delete=models.CASCADE)
+#     service = models.ForeignKey("services.Service", on_delete=models.CASCADE)
 
 
-class Like_User(models.Model):
-    favoriting_user = models.ForeignKey("users.User", on_delete=models.CASCADE, related_name="favoriting_user")
-    favorited_user = models.ForeignKey("users.User", on_delete=models.CASCADE, related_name="favorited_user")
-    date_created = models.DateTimeField(default=timezone.now)
+# class Like_User(models.Model):
+#     favoriting_user = models.ForeignKey("users.User", on_delete=models.CASCADE, related_name="favoriting_user")
+#     favorited_user = models.ForeignKey("users.User", on_delete=models.CASCADE, related_name="favorited_user")
+#     date_created = models.DateTimeField(default=timezone.now)
 
-class View_User(models.Model):
-    viewing_user = models.ForeignKey("users.User", on_delete=models.CASCADE, related_name="viewing_user")
-    viewed_user = models.ForeignKey("users.User", on_delete=models.CASCADE, related_name="viewed_user")
+# class View_User(models.Model):
+#     viewing_user = models.ForeignKey("users.User", on_delete=models.CASCADE, related_name="viewing_user")
+#     viewed_user = models.ForeignKey("users.User", on_delete=models.CASCADE, related_name="viewed_user")
 
-class Rate_User(models.Model):
-    rating_user = models.ForeignKey("users.User", on_delete=models.CASCADE, related_name="rating_user")
-    rated_user = models.ForeignKey("users.User", on_delete=models.CASCADE, related_name="rated_user")
-    rate_number = models.IntegerField()
-    description = models.TextField()
-    image = models.ImageField(upload_to='rate/%Y/%m/', max_length=255)
-    date_created = models.DateTimeField(default=timezone.now)
+# class Rate_User(models.Model):
+#     rating_user = models.ForeignKey("users.User", on_delete=models.CASCADE, related_name="rating_user")
+#     rated_user = models.ForeignKey("users.User", on_delete=models.CASCADE, related_name="rated_user")
+#     rate_number = models.IntegerField()
+#     description = models.TextField()
+#     image = models.ImageField(upload_to='rate/%Y/%m/', max_length=255)
+#     date_created = models.DateTimeField(default=timezone.now)
 
-    def __str__(self):
-        return f'{self.rating_user} rated {self.rated_user} with {self.rate_number} star.'
+#     def __str__(self):
+#         return f'{self.rating_user} rated {self.rated_user} with {self.rate_number} star.'
