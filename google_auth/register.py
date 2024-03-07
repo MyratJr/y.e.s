9c6b199026aa3e7b3237bbd88d39946ab5af7c78
Begmyradov Myratgeldi <begmyradowmyratgeldi@gmail.com>
2024-03-07 22:00:31 +0500

    uncomment user

--- google_auth/register.py
+++ google_auth/register.py
@@ -1,45 +1,45 @@
-from users.models import User
-import os
-from rest_framework.exceptions import AuthenticationFailed
-from django.contrib.auth import authenticate
-import random
-from string import ascii_lowercase
+# from users.models import User
+# import os
+# from rest_framework.exceptions import AuthenticationFailed
+# from django.contrib.auth import authenticate
+# import random
+# from string import ascii_lowercase
 
 
-def generate_username(user_id):
-    alphabet = ascii_lowercase
-    random_letters = random.sample(alphabet, 5)
-    return ''.join(random_letters)+str(user_id)
+# def generate_username(user_id):
+#     alphabet = ascii_lowercase
+#     random_letters = random.sample(alphabet, 5)
+#     return ''.join(random_letters)+str(user_id)
 
 
-def register_social_user(provider, user_id, email):
-    filtered_user_by_email = User.objects.filter(email=email)
+# def register_social_user(provider, user_id, email):
+#     filtered_user_by_email = User.objects.filter(email=email)
     
-    if filtered_user_by_email.exists():
-        if provider == filtered_user_by_email[0].registration_method:
-            authenticate(email=email, password=os.environ.get('SOCIAL_AUTH_PASSWORD'))
-            return {
-                "username": filtered_user_by_email[0].username,
-                "email": filtered_user_by_email[0].email, 
-                "tokens": filtered_user_by_email[0].tokens()  
-            }
-        else:
-            raise AuthenticationFailed(
-                detail="Please continue your login using" + filtered_user_by_email[0].registration_method
-            )
+#     if filtered_user_by_email.exists():
+#         if provider == filtered_user_by_email[0].registration_method:
+#             authenticate(email=email, password=os.environ.get('SOCIAL_AUTH_PASSWORD'))
+#             return {
+#                 "username": filtered_user_by_email[0].username,
+#                 "email": filtered_user_by_email[0].email, 
+#                 "tokens": filtered_user_by_email[0].tokens()  
+#             }
+#         else:
+#             raise AuthenticationFailed(
+#                 detail="Please continue your login using" + filtered_user_by_email[0].registration_method
+#             )
         
-    else:
-        user = {
-            'username': generate_username(user_id), 'email': email,
-            'password': os.environ.get('SOCIAL_AUTH_PASSWORD')}
-        user = User.objects.create_user(**user)
-        user.registration_method = provider
-        user.save()
+#     else:
+#         user = {
+#             'username': generate_username(user_id), 'email': email,
+#             'password': os.environ.get('SOCIAL_AUTH_PASSWORD')}
+#         user = User.objects.create_user(**user)
+#         user.registration_method = provider
+#         user.save()
 
-        authenticate(
-            email=email, password=os.environ.get('SOCIAL_AUTH_PASSWORD'))
-        return {
-            'email': user.email,
-            'username': user.username,
-            'tokens': user.tokens()
-        }
+#         authenticate(
+#             email=email, password=os.environ.get('SOCIAL_AUTH_PASSWORD'))
+#         return {
+#             'email': user.email,
+#             'username': user.username,
+#             'tokens': user.tokens()
+#         }
