e142256bbf3d3dadb929d534dfc6ce9ff1d7c491
Begmyradov Myratgeldi <begmyradowmyratgeldi@gmail.com>
2024-03-07 21:59:42 +0500

    uncomment user

--- otp/views.py
+++ otp/views.py
@@ -1,41 +1,41 @@
-from rest_framework import status
-from rest_framework.response import Response
-from .serializers import OTPSerializer
-from random import randint
-from rest_framework import mixins, generics
-from rest_framework.parsers import MultiPartParser
-from rest_framework.permissions import AllowAny
-from users.models import User
-from ehyzmat.settings import redis_cache
+# from rest_framework import status
+# from rest_framework.response import Response
+# from .serializers import OTPSerializer
+# from random import randint
+# from rest_framework import mixins, generics
+# from rest_framework.parsers import MultiPartParser
+# from rest_framework.permissions import AllowAny
+# from users.models import User
+# from ehyzmat.settings import redis_cache
    
 
-class OTPView(mixins.CreateModelMixin, generics.GenericAPIView):
-    permission_classes = [AllowAny]
-    serializer_class = OTPSerializer
-    parser_classes = [MultiPartParser]
+# class OTPView(mixins.CreateModelMixin, generics.GenericAPIView):
+#     permission_classes = [AllowAny]
+#     serializer_class = OTPSerializer
+#     parser_classes = [MultiPartParser]
 
-    def post(self, request, *args, **kwargs):
-        serializer = self.serializer_class(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        phone = serializer.validated_data['phone']
-        otp = randint(1000,9999)
-        redis_cache.set(phone, otp, ex=300)
-        return Response({"phone":phone, "otp":otp}, status=status.HTTP_201_CREATED)
+#     def post(self, request, *args, **kwargs):
+#         serializer = self.serializer_class(data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         phone = serializer.validated_data['phone']
+#         otp = randint(1000,9999)
+#         redis_cache.set(phone, otp, ex=300)
+#         return Response({"phone":phone, "otp":otp}, status=status.HTTP_201_CREATED)
     
 
-class ForgotPasswordView(mixins.CreateModelMixin, generics.GenericAPIView):
-    permission_classes = [AllowAny]
-    serializer_class = OTPSerializer
-    parser_classes = [MultiPartParser]
+# class ForgotPasswordView(mixins.CreateModelMixin, generics.GenericAPIView):
+#     permission_classes = [AllowAny]
+#     serializer_class = OTPSerializer
+#     parser_classes = [MultiPartParser]
 
-    def post(self, request, *args, **kwargs):
-        serializer = self.serializer_class(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        phone = serializer.validated_data['phone']
-        try: 
-            user_with_given_phone = User.objects.get(phone=phone)
-        except:
-            return Response({"No user found with this phone number"})
-        otp = randint(1000,9999)
-        redis_cache.set(phone, otp, ex=300)
-        return Response({"user_id":user_with_given_phone.id, "phone":phone, "otp":otp}, status=status.HTTP_201_CREATED)
+#     def post(self, request, *args, **kwargs):
+#         serializer = self.serializer_class(data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         phone = serializer.validated_data['phone']
+#         try: 
+#             user_with_given_phone = User.objects.get(phone=phone)
+#         except:
+#             return Response({"No user found with this phone number"})
+#         otp = randint(1000,9999)
+#         redis_cache.set(phone, otp, ex=300)
+#         return Response({"user_id":user_with_given_phone.id, "phone":phone, "otp":otp}, status=status.HTTP_201_CREATED)
