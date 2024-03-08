fc1e57871f6465332d2654ea129395d0418b1236
Begmyradov Myratgeldi <begmyradowmyratgeldi@gmail.com>
2024-03-08 23:25:00 +0500

    fix migrations

--- ratings/views.py
+++ ratings/views.py
@@ -32,10 +32,10 @@ class RateUserView(mixins.CreateModelMixin, generics.GenericAPIView):
         serializer.is_valid(raise_exception=True)
         rated_user = serializer.validated_data.get('rated_user')
         rated_user = get_object_or_404(User, pk=rated_user.id)
-        # rating_user = request.user
+        rating_user = request.user
         rated_user.point_counter += 1
         rated_user.rate_point_total += serializer.validated_data.get('rate_number')
         rated_user.rate_point = rated_user.rate_point_total / rated_user.point_counter
         rated_user.save()
-        self.perform_create(serializer)
+        self.perform_create(serializer, rating_user)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
