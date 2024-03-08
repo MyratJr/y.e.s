a3c2096e202e49826d0346745d41f83370bd579e
Begmyradov Myratgeldi <begmyradowmyratgeldi@gmail.com>
2024-03-08 23:12:11 +0500

    make in order

--- ratings/serializers.py
+++ ratings/serializers.py
@@ -27,6 +27,8 @@ class RatedUserSerializer(serializers.ModelSerializer):
 
 
 class Rate_User_Serializer(serializers.ModelSerializer):
+    rating_user = serializers.IntegerField(read_only=True)
+    
     class Meta:
         model = Rate_User
         fields = ["rated_user", "rate_number", "description", "image"]
