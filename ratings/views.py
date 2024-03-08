9fc10d21e8ce664e67f8ee61272f92b06aaea9fb
Begmyradov Myratgeldi <begmyradowmyratgeldi@gmail.com>
2024-03-08 23:26:51 +0500

    make in order

--- ratings/views.py
+++ ratings/views.py
@@ -36,5 +36,5 @@ class RateUserView(mixins.CreateModelMixin, generics.GenericAPIView):
         rated_user.rate_point_total += serializer.validated_data.get('rate_number')
         rated_user.rate_point = rated_user.rate_point_total / rated_user.point_counter
         rated_user.save()
-        self.perform_create(serializer, rating_user)
+        self.perform_create(serializer)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
