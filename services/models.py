a582d0821f8326d2fe7102da8cec99a8bd7cefe1
Begmyradov Myratgeldi <begmyradowmyratgeldi@gmail.com>
2024-03-09 23:05:09 +0500

    make in order

--- services/models.py
+++ services/models.py
@@ -12,7 +12,7 @@ class Service_Category(models.Model):
 
 
 class Service(models.Model):
-    user = models.ForeignKey("users.User", on_delete=models.CASCADE, related_name='service_user')
+    user = models.ForeignKey("users.User", on_delete=models.CASCADE, related_name='service_user', null=True)
     name = models.CharField(max_length=25)
     price = models.DecimalField(max_digits=7, decimal_places=2)
     category = models.ForeignKey(Service_Category, on_delete=models.CASCADE, related_name='service_category')
