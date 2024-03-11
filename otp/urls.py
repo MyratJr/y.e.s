7b597bf8c70f171db5dedd0d77e7fc9d4051ce8d
Begmyradov Myratgeldi <begmyradowmyratgeldi@gmail.com>
2024-03-11 17:41:05 +0500

    make in order

--- otp/urls.py
+++ otp/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('post_otp/', views.OTPView.as_view(), name='otp'),
-    # path('forgot-password/', views.ForgotPasswordView.as_view(), name='forgot_password'),
+    path('post_otp/', views.OTPView.as_view(), name='otp'),
+    path('forgot-password/', views.ForgotPasswordView.as_view(), name='forgot_password'),
 ]
