44c8894f80e510d22cd637a35ce4fbf3e0d9ddbf
Begmyradov Myratgeldi <begmyradowmyratgeldi@gmail.com>
2024-03-11 22:48:17 +0500

    make in order

--- ehyzmat/celery.py
+++ ehyzmat/celery.py
@@ -15,6 +15,10 @@ app.conf.beat_schedule = {
         'task': 'advertisement.tasks.advertisement_vip_task',
         'schedule': crontab(hour=0, minute=0),
     },
+    'OTP_task': {
+        'task': 'otp.tasks.otp_task',
+        'schedule': crontab(),
+    },
     'flushexpiredtokens_task': {
         'task': 'users.tasks.flushexpiredtokens_t',
         'schedule': crontab(hour=0, minute=0),
