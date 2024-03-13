5d2e67114c2999cc8608e76ff4783e83d3f8eaba
Begmyradov Myratgeldi <begmyradowmyratgeldi@gmail.com>
2024-03-13 19:38:17 +0500

    make in order

--- users/serializers.py
+++ users/serializers.py
@@ -163,7 +163,7 @@ class LikeToServiceSerializer(serializers.ModelSerializer):
 
 
 class LikedServiceSerializer(serializers.ModelSerializer):
-    user = LikesFromUsersSerializer()
+    user = LikeToUserSerializer()
     service = LikeToServiceSerializer()
 
     class Meta:
