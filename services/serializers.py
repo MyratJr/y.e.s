75726de5cc81123fbbffbf8a9659ff5ba5b90111
Begmyradov Myratgeldi <begmyradowmyratgeldi@gmail.com>
2024-03-14 18:39:05 +0500

    make in order

--- services/serializers.py
+++ services/serializers.py
@@ -47,7 +47,7 @@ class ServicesSerializers(serializers.ModelSerializer):
     def create(self, validated_data):
         uploaded_images_id = validated_data.pop("uploaded_images")
         new_service = Service.objects.create(**validated_data)
-        for relating_image in ServiceGalleryImage.objects.get(id__in=eval(uploaded_images_id[0])):
+        for relating_image in ServiceGalleryImage.objects.filter(id__in=eval(uploaded_images_id[0])):
             relating_image.product = new_service
             relating_image.save()
         return new_service
