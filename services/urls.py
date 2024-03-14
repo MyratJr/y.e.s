f17e8f4a8039c6a823879278fe74df6acffd75ae
Begmyradov Myratgeldi <begmyradowmyratgeldi@gmail.com>
2024-03-14 19:03:27 +0500

    make in order

--- services/urls.py
+++ services/urls.py
@@ -10,7 +10,7 @@ router.register('services-view', views.Services_View, basename='services_view')
 urlpatterns = [
     path('', include(router.urls)),
     path('service_gallery_image/', views.Service_Gallery_ImagesAPIView.as_view(), name='service-gallery-images'),
-    path('service_gallery_destroy/<int:pk>', views.Service_Gallery_DestroyView.as_view(), name='service-gallery-destroy'),
+    path('service_gallery_destroy/<int:pk>/', views.Service_Gallery_DestroyView.as_view(), name='service-gallery-destroy'),
     path('home/', views.HomeDataView.as_view(), name='home-datas'),
     path('categories_list/', views.All_CategoriesAPIView.as_view(), name='categories'),
     path('like_service/<int:pk>', views.LikeServiceAPIView.as_view(), name='like-user'),
