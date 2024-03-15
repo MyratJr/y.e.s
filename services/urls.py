13cc78592e6039b6d37172f276cbbac8496ce247
Begmyradov Myratgeldi <begmyradowmyratgeldi@gmail.com>
2024-03-15 12:05:14 +0500

    make in order

--- services/urls.py
+++ services/urls.py
@@ -15,6 +15,6 @@ urlpatterns = [
     path('like_service/<int:pk>', views.LikeServiceAPIView.as_view(), name='like-user'),
     path('filter-service', views.FilterServiceList.as_view(), name='filter_service'),
     path('home-services', views.HomeServicesView.as_view(), name='home_services'),
-    path('home-service-categories', views.HomeServiceCategoriesView, name='home_service_categories'),
-    path('home-advertisement', views.HomeAdvertisementView, name='home_advertisements')
+    path('home-service-categories', views.HomeServiceCategoriesView.as_view(), name='home_service_categories'),
+    path('home-advertisement', views.HomeAdvertisementView.as_view(), name='home_advertisements')
 ]
