4f78d406e98042a041e4f0512dfcd5868166ffd4
Begmyradov Myratgeldi <begmyradowmyratgeldi@gmail.com>
2024-03-16 18:25:31 +0500

    make in order

--- advertisement/admin.py
+++ advertisement/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Advertisement
 
-
-admin.site.register(Advertisement)
+@admin.register(Advertisement)
+class AdvertisementAdmin(admin.ModelAdmin):
+    list_display = ('title', 'image', 'is_active', 'expired_date', 'date_created')
