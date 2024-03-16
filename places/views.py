e2bcc0fe4b2cc1d0908862bee1dcf4cd287f6812
Begmyradov Myratgeldi <begmyradowmyratgeldi@gmail.com>
2024-03-16 12:54:58 +0500

    make in order

--- places/views.py
+++ places/views.py
@@ -6,7 +6,7 @@ from .models import Regions
 
 class SnippetList(APIView):
     
-    def get(self):
+    def get(self, request):
         snippets = Regions.objects.all()
         serializer = RegionsSerializer(snippets, many=True)
         return Response(serializer.data)
