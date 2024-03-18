6f243b65a107feed06f9cfcafd54524ee4d31526
Begmyradov Myratgeldi <begmyradowmyratgeldi@gmail.com>
2024-03-18 11:28:06 +0500

    make in order

--- ehyzmat/settings.py
+++ ehyzmat/settings.py
@@ -73,7 +73,7 @@ ROOT_URLCONF = 'ehyzmat.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates/admin'],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
