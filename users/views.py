9760a5e857964f3b6334c4dc228a9abc02147f7e
Begmyradov Myratgeldi <begmyradowmyratgeldi@gmail.com>
2024-03-19 18:34:41 +0500

    otp changes

--- users/views.py
+++ users/views.py
@@ -28,7 +28,7 @@ class RegisterAPI(generics.GenericAPIView):
         serializer.save()
         phone = serializer.validated_data["phone"]
         otp = randint(10000,99999)
-        redis_cache.set(phone, otp, ex=600)
+        redis_cache.set(phone, otp)
         Otp.objects.create(
             phone=phone,
             message=f"Siziň barlag koduňyz: {otp}"
