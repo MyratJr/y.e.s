3ab634fec6fcf162a7f7815e1cabd5fd67800a5d
Begmyradov Myratgeldi <begmyradowmyratgeldi@gmail.com>
2024-03-19 09:24:06 +0500

    make in order

--- ehyzmat/urls.py
+++ ehyzmat/urls.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import permissions
-from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from django.conf import settings
 from rest_framework_simplejwt.views import TokenRefreshView, TokenBlacklistView
@@ -9,26 +8,13 @@ from users.views import RegisterAPI, LoginAPI
 from django.conf.urls.static import static
 
 
-schema_view = get_schema_view(
-    openapi.Info(
-        title="Y.E.S API",
-        default_version='v1',
-        description="Welcome to the Y.E.S API documentation",
-    ),
-    public=True,
-    permission_classes=(permissions.AllowAny,),
-)
-
-
-urlpatterns = [
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0),name='schema-swagger-ui'),
+urlpatterns1 = [
     path('admin/', admin.site.urls),
     path('user/', include('users.urls')),
     path('region/', include('places.urls')),
     path('service/', include('services.urls')),
     path('advertisement/', include('advertisement.urls')),
     path('ratings/', include('ratings.urls')),
-    path('otp/', include('otp.urls'), name="otp_view"),
     path('auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('auth/register/', RegisterAPI.as_view(), name='register'),
     path('auth/login/', LoginAPI.as_view(), name='login'),
@@ -38,4 +24,24 @@ urlpatterns = [
 
 
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns1 += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+from drf_yasg.views import get_schema_view
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Y.E.S API",
+        default_version='v1',
+        description="Welcome to the Y.E.S API documentation",
+    ),
+    patterns=urlpatterns1,
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+
+urlpatterns = urlpatterns1 + [
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0),name='schema-swagger-ui'),
+    path('otp/', include('otp.urls'), name="otp_view"),
+]
