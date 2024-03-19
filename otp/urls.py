bef6f5c848929786f1ecbe1b1ed09f269e292e39
Begmyradov Myratgeldi <begmyradowmyratgeldi@gmail.com>
2024-03-19 18:22:29 +0500

    otp changes

--- otp/urls.py
+++ otp/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('post_otp/', views.OTPView.as_view(), name='otp'),
     path('forgot-password/', views.ForgotPasswordView.as_view(), name='forgot_password'),
-    path('sms-phone', views.SMSPhoneView, name="sms_phone"),
-    path('activate-user', views.ActivateUserAPIView, name="activate_user"),
+    path('sms-phone', views.SMSPhoneView.as_view(), name="sms_phone"),
+    path('activate-user', views.ActivateUserAPIView.as_view(), name="activate_user"),
 ]
