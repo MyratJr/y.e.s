596a3914e35bd59882ea61eaeda720dfc137fcac
Begmyradov Myratgeldi <begmyradowmyratgeldi@gmail.com>
2024-03-19 09:57:27 +0500

    make in order

--- otp/urls.py
+++ otp/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('post_otp/', views.OTPView.as_view(), name='otp'),
     path('forgot-password/', views.ForgotPasswordView.as_view(), name='forgot_password'),
+    path('list-phones', views.ListPhoneNumbersView.as_view(), name='list_phones')
 ]
--- otp/views.py
+++ otp/views.py
@@ -4,10 +4,11 @@ from .serializers import OTPSerializer
 from random import randint
 from rest_framework import mixins, generics
 from rest_framework.parsers import MultiPartParser
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from users.models import User
 from ehyzmat.settings import redis_cache
-   
+from rest_framework.views import APIView
+
 
 class OTPView(mixins.CreateModelMixin, generics.GenericAPIView):
     permission_classes = [AllowAny]
@@ -20,7 +21,7 @@ class OTPView(mixins.CreateModelMixin, generics.GenericAPIView):
         phone = serializer.validated_data['phone']
         otp = randint(1000,9999)
         redis_cache.set(phone, otp, ex=300)
-        return Response({"phone":phone, "otp":otp}, status=status.HTTP_201_CREATED)
+        return Response(status=status.HTTP_201_CREATED)
     
 
 class ForgotPasswordView(mixins.CreateModelMixin, generics.GenericAPIView):
@@ -38,4 +39,15 @@ class ForgotPasswordView(mixins.CreateModelMixin, generics.GenericAPIView):
             return Response({"No user found with this phone!"})
         otp = randint(1000,9999)
         redis_cache.set(phone, otp, ex=300)
-        return Response({"phone":phone, "otp":otp}, status=status.HTTP_201_CREATED)
+        return Response(status=status.HTTP_201_CREATED)
+    
+
+class ListPhoneNumbersView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, *args, **kwargs):
+        phone_numbers = []
+        for key, value in redis_cache.items():
+            if isinstance(value, int):
+                phone_numbers.append({'phone_number': key, 'otp': value})
+        return Response(phone_numbers)
