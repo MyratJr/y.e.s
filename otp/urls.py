c0e27e416dc28ba5818282ac73b1ab8632811b51
Begmyradov Myratgeldi <begmyradowmyratgeldi@gmail.com>
2024-03-21 17:04:37 +0500

    make in order

--- otp/urls.py
+++ otp/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('resend-otp/', views.ResendOTPView.as_view(), name='resend_otp'),
-    path('forgot-password/', views.ForgotPasswordView.as_view(), name='forgot_password'),
+    # path('forgot-password/', views.ForgotPasswordView.as_view(), name='forgot_password'),
     path('sms-phone/', views.SMSPhoneView.as_view(), name="sms_phone"),
     path('activate-user/', views.ActivateUserAPIView.as_view(), name="activate_user"),
 ]
