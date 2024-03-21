e64ab1a27a1ba64aa97162f62d77759943312f3c
Begmyradov Myratgeldi <begmyradowmyratgeldi@gmail.com>
2024-03-21 22:33:43 +0500

    make in order

--- services/admin.py
+++ services/admin.py
@@ -16,6 +16,6 @@ class CarAdmin(admin.ModelAdmin):
     inlines = [PropertyVideoInline]
     def get_new_icon(self, obj):
         if obj.is_new:
-            return format_html('<img src="{}">'.format(MEDIA_URL + 'service/service_images/new.png'))
+            return format_html('<img src="{}"> width="40"'.format(MEDIA_URL + 'service/service_images/new.png'))
         else:
             return ''
