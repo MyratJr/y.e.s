df504977d2b2280dd0bdf727d3cf996dafdffdab
Begmyradov Myratgeldi <begmyradowmyratgeldi@gmail.com>
2024-03-21 23:31:47 +0500

    make in order

--- services/admin.py
+++ services/admin.py
@@ -19,3 +19,10 @@ class CarAdmin(admin.ModelAdmin):
             return format_html('<img src="{}" width="30" height="30" style="position:fixed; margin-top:-0.5%">'.format(MEDIA_URL + 'service/service_images/new.png'))
         else:
             return ''
+    
+    def changeform_view(self, request, object_id=None, form=None, extra_context=None):
+        if object_id:
+            product = Service.objects.get(pk=object_id)
+            product.is_new = True
+            product.save()
+        return super().changeform_view(request, object_id, form, extra_context)
