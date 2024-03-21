9749f1dd83674e81c31bb3e6d4fa4db61ddd32ab
Begmyradov Myratgeldi <begmyradowmyratgeldi@gmail.com>
2024-03-21 22:32:35 +0500

    make in order

--- services/admin.py
+++ services/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import *
 from django.utils.html import format_html
+from ehyzmat.settings import MEDIA_URL
 
 class PropertyVideoInline(admin.StackedInline):
     model = ServiceGalleryImage
@@ -15,6 +16,6 @@ class CarAdmin(admin.ModelAdmin):
     inlines = [PropertyVideoInline]
     def get_new_icon(self, obj):
         if obj.is_new:
-            return format_html('<img src="../../otp/new.png">')
+            return format_html('<img src="{}">'.format(MEDIA_URL + 'service/service_images/new.png'))
         else:
             return ''
