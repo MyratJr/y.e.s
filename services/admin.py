6ac37458a6a81741702244833ee6d83413fd4d55
Begmyradov Myratgeldi <begmyradowmyratgeldi@gmail.com>
2024-03-23 18:36:39 +0500

    make in order

--- services/admin.py
+++ services/admin.py
@@ -12,10 +12,10 @@ admin.site.register(Service_Category)
 
 @admin.register(Service)
 class CarAdmin(admin.ModelAdmin):
-    list_display = ['name', 'category', 'place', 'public']
+    list_display = ['name', 'category', 'place', 'public', 'new']
     inlines = [PropertyVideoInline]
     
-    def public(self, obj):
+    def new(self, obj):
         if obj.is_new:
             return format_html('<img src="{}" width="30" height="30" style="position:fixed; margin-top:-0.5%">'.format(MEDIA_URL + 'service/service_images/new.png'))
         else:
