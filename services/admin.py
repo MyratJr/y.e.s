78a1092493019b8f1740ba31293e7a006267d0fe
Begmyradov Myratgeldi <begmyradowmyratgeldi@gmail.com>
2024-03-23 20:33:42 +0500

    make in order

--- services/admin.py
+++ services/admin.py
@@ -12,11 +12,5 @@ admin.site.register(Service_Category)
 
 @admin.register(Service)
 class CarAdmin(admin.ModelAdmin):
-    list_display = ['name', 'category', 'place', "statusa"]
+    list_display = ['name', 'category', 'place', "status"]
     inlines = [PropertyVideoInline]
-    
-    def statusa(self, obj):
-        if obj.status=="Failed":
-            return format_html('<img src="{}" width="30" height="30" style="position:fixed; margin-top:-0.5%">'.format(MEDIA_URL + 'service/service_images/new.png'))
-        else:
-            return ''
