b240e22e9eee0d8e1ada364a4c231c4a8df5644c
Begmyradov Myratgeldi <begmyradowmyratgeldi@gmail.com>
2024-03-23 20:50:30 +0500

    make in order

--- services/admin.py
+++ services/admin.py
@@ -6,7 +6,7 @@ from ehyzmat.settings import MEDIA_URL
 class PropertyVideoInline(admin.StackedInline):
     model = ServiceGalleryImage
 
-
+# format_html('<img src="{}" width="30" height="30" style="position:fixed; margin-top:-0.5%">'.format(MEDIA_URL + 'service/service_images/new.png'))
 admin.site.register(Service_Category)
 
 
@@ -14,3 +14,13 @@ admin.site.register(Service_Category)
 class CarAdmin(admin.ModelAdmin):
     list_display = ['name', 'category', 'place', "status"]
     inlines = [PropertyVideoInline]
+    
+    def status(self, obj):
+        if obj.status==ServiceVerification.Garashylyar:
+            return "Garaşylýar"
+        elif obj.status==ServiceVerification.Showsyz:
+            return "Şowsuz"
+        elif obj.status==ServiceVerification.Kabul_Edildi:
+            return "Kabul edildi"
+        else:
+            return ''
