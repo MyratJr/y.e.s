2d26cf2af08ba55a76c0da08eea61c8ec45622b0
Begmyradov Myratgeldi <begmyradowmyratgeldi@gmail.com>
2024-03-24 19:07:40 +0500

    make in order

--- services/serializers.py
+++ services/serializers.py
@@ -34,20 +34,19 @@ class ServicesSerializers(serializers.ModelSerializer):
     user = serializers.SerializerMethodField(read_only=True)
     view_counter = serializers.IntegerField(read_only=True)
     like_counter = serializers.IntegerField(read_only=True)
-    category_name = serializers.SerializerMethodField(read_only=True)
-    category = serializers.UUIDField(write_only=True)
-    place_name = serializers.SerializerMethodField(read_only=True)
-    place = serializers.UUIDField(write_only=True)
+    category_name = serializers.CharField(source='category.name', read_only=True)
+    # category = serializers.UUIDField(write_only=True)
+    place_name = serializers.CharField(source='place.district', read_only=True)
+    # place = serializers.UUIDField(write_only=True)
     status = serializers.CharField(read_only=True)
     vip_date = serializers.DateField(read_only=True)
     vip_is_active = serializers.BooleanField(read_only=True)
 
-
-    def get_category_name(self, obj):
-        return Service_Category.objects.get(id=(obj.category.id)).name
+    # def get_category_name(self, obj):
+    #     return Service_Category.objects.get(id=(obj.category.id)).name
     
-    def get_place_name(self, obj):
-        return Districts.objects.get(id=(obj.place.id)).district
+    # def get_place_name(self, obj):
+    #     return Districts.objects.get(id=(obj.place.id)).district
 
     def get_user(self, obj):
         request = self.context["request"]
@@ -68,9 +67,9 @@ class ServicesSerializers(serializers.ModelSerializer):
                   "user", 
                   "name", 
                   "price", 
-                  "place", 
+                  "place_id", 
                   "place_name", 
-                  "category", 
+                  "category_id", 
                   "category_name",
                   "experience", 
                   "description", 
@@ -86,12 +85,12 @@ class ServicesSerializers(serializers.ModelSerializer):
     
     def create(self, validated_data):
         uploaded_images_id = validated_data.pop("uploaded_images")
-        category_uuid = validated_data.pop('category')
-        place_uuid = validated_data.pop('place')
-        category_object = Service_Category.objects.get(pk=category_uuid)
-        place_object = Districts.objects.get(pk=place_uuid)
-        validated_data['category'] = category_object
-        validated_data['place'] = place_object
+        # category_uuid = validated_data.pop('category')
+        # place_uuid = validated_data.pop('place')
+        # category_object = Service_Category.objects.get(pk=category_uuid)
+        # place_object = Districts.objects.get(pk=place_uuid)
+        # validated_data['category'] = category_object
+        # validated_data['place'] = place_object
         new_service = Service.objects.create(**validated_data)
         for relating_image in ServiceGalleryImage.objects.filter(id__in=eval(uploaded_images_id[0])):
             relating_image.product = new_service
@@ -100,12 +99,12 @@ class ServicesSerializers(serializers.ModelSerializer):
     
     def update(self, instance, validated_data):
         uploaded_images_id = validated_data.pop("uploaded_images")
-        category_uuid = validated_data.pop('category')
-        place_uuid = validated_data.pop('place')
-        category_object = Service_Category.objects.get(pk=category_uuid)
-        place_object = Districts.objects.get(pk=place_uuid)
-        validated_data['category'] = category_object
-        validated_data['place'] = place_object
+        # category_uuid = validated_data.pop('category')
+        # place_uuid = validated_data.pop('place')
+        # category_object = Service_Category.objects.get(pk=category_uuid)
+        # place_object = Districts.objects.get(pk=place_uuid)
+        # validated_data['category'] = category_object
+        # validated_data['place'] = place_object
         for field, value in validated_data.items():
             setattr(instance, field, value)
         instance.save()
